85b3afced3c5a38ad6eb671af962d3cabda684f1
Julia Bugayev <jbugayev18@gmail.com>
2021-02-13 13:44:31 -0800

    Woking on material-ui features

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -3,13 +3,14 @@ import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import DriveEtaIcon from "@material-ui/icons/DriveEta";
+import "fontsource-roboto";
 
 const NavBar = () => {
   return (
     <div>
       <AppBar position="static">
         <ToolBar>
-          <Typography variant="title" color="inherit">
+          <Typography variant="title" component="h3" color="inherit">
             HypeCar Sample Presentation <DriveEtaIcon />
           </Typography>
         </ToolBar>
--- src/cars.js
+++ src/cars.js
@@ -1,15 +1,39 @@
+import { Grid } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 import React from "react";
 
 export default function Car(props) {
   return (
     <div className="car">
-      <h2>{props.make}</h2>
-      <div className="model">Model: {props.model}</div>
-      <div className="year">Year of model: {props.year}</div>
-      <div className="color">Color: {props.color}</div>
-      <div className="Odometer"> Odometer: {props.Odometer}</div>
-      <div className="VIN"> VIN: {props.VIN}</div>
-      <img alt="car" src={props.image}></img>
+      <Grid container spacing={6}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          item
+          xs={12}
+        >
+          <Box
+            bgcolor="primary.main"
+            color="primary.contrastText"
+            p={3}
+            flexDirection="column"
+            borderRadius={16}
+          >
+            <h2>{props.make}</h2>
+            <div className="model">Model: {props.model}</div>
+            <div className="year">Year of model: {props.year}</div>
+            <div className="color">Color: {props.color}</div>
+            <div className="Odometer"> Odometer: {props.Odometer}</div>
+            <div className="VIN"> VIN: {props.VIN}</div>
+
+            <Box p={3}>
+              <img alt="cars" width="700" src={props.image}></img>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 }
